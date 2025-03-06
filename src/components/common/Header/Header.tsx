a2edfd0f646498a1645a0419b2107e68428349f5
nick_romanov <nikolay.romanov164@gmail.com>
2025-03-06 16:54:26 +0300

    Added header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,19 +1,47 @@
-import { NAVIGATION_ITEMS } from "./const";
-import styles from "./Header.module.scss";
+import React from "react";
+
+import { cn } from "@/lib/utils";
+
+const NAV_LINKS = [
+  { name: "Main", path: "/" },
+  { name: "Audi", path: "/audi" },
+  { name: "Bmw", path: "/bmw" },
+];
 
 export function Header() {
+  const [currentPath, setCurrentPath] = React.useState("");
+
+  React.useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
+
+  function handleLogoClick() {
+    window.location.href = "/";
+  }
+
   return (
-    <div className={styles.base}>
-      <div className={styles.logo}>LOGO</div>
-      <div className={styles.navigation}>
-        {NAVIGATION_ITEMS.map((item) => {
-          return (
-            <a key={item.link} href={item.link}>
-              {item.label}
-            </a>
-          );
-        })}
-      </div>
-    </div>
+    <header className="flex items-center justify-between px-6 py-4 bg-white shadow-md rounded-b-lg">
+      <h1
+        onClick={handleLogoClick}
+        className="text-xl font-bold cursor-pointer select-none"
+      >
+        Cars
+      </h1>
+
+      <nav className="flex gap-6">
+        {NAV_LINKS.map((link) => (
+          <a
+            key={link.path}
+            href={link.path}
+            className={cn(
+              "text-gray-600 hover:text-black transition",
+              currentPath === link.path && "text-black font-semibold underline",
+            )}
+          >
+            {link.name}
+          </a>
+        ))}
+      </nav>
+    </header>
   );
 }
--- src/components/pages/Cars/Cars.tsx
+++ src/components/pages/Cars/Cars.tsx
@@ -36,7 +36,7 @@ export function Cars(props: Props) {
   }, [filters, props.cars]);
 
   return (
-    <div className="p-12 flex flex-col gap-y-12">
+    <div className="p-4 flex flex-col gap-y-12">
       <Search />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {cars.map((car) => (
